9490e41f0adfb90f60ff0217548e5dfb127fa0ea
jonatan <jonatan5524@gmail.com>
2021-10-29 17:01:05 +0300

    loading 32 bit kernel into memory

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -43,8 +43,11 @@ start_boot:
   mov cr0, eax
   ; switch to code selector and jmp to load32 address
   jmp CODE_SEG:load32 
+  jmp $
 
 ; GDT - Global Descriptor Table
+; Base - linear address of the segment
+; Limit - Size of the segment 
 gdt_start:
 gdt_null:
   dd 0x0
@@ -90,18 +93,76 @@ gdt_descriptor:
   ; gdt start address
   dd gdt_start 
 
-; set 32 bit code 
 [BITS 32]
 load32:
-  mov ax, DATA_SEG
-  mov ds, ax
-  mov es, ax
-  mov fs, ax
-  mov gs, ax
-  mov ss, ax
-  mov ebp, 0x00200000
-  mov esp, ebp
-  jmp $
+  ; sector to read from
+  mov eax, 1
+  ; number of sector to read 
+  mov ecx, 100
+  ; address to load into 
+  mov edi, 0x0100000
+  call ata_lba_read
+  jmp CODE_SEG:0x0100000
+
+ata_lba_read:
+  ; backup the lba
+  mov ebx, eax
+  ; send the highest 8 bits of the lba to hard disk controller
+  shr eax, 24
+  ; select the master drive
+  or eax, 0xE0
+  ; port to write the 8 bits to 
+  mov dx, 0x1F6
+  out dx, al
+  ; finished sending highest 8 bits of the lba
+
+  ; send the total sector to read 
+  mov eax, ecx
+  mov dx, 0x1F2
+  out dx, al 
+  
+  ; send more bits to the lba
+  ; retore the backyp lba 
+  mov eax, ebx
+  mov dx, 0x1F3
+  out dx, al
+
+  ; send more bits of the lba
+  mov dx, 0x1F4
+  ; restore the backup lba
+  mov eax, ebx
+  shr eax, 8
+  out dx, al
+
+  ; send upper 16 bits of the lba
+  mov dx, 0x1F5
+  ; restore the backup lba
+  mov eax, ebx 
+  shr eax, 16 
+  out dx, al
+
+  mov dx, 0x1F7
+  mov al, 0x20
+  out dx, al 
+
+  ; read all sectors into memory
+.next_sector:
+  push ecx
+
+; checking if we need to read
+.try_again:
+  mov dx, 0x1F7
+  in al,dx
+  test al, 8
+  jz .try_again
+
+; read 256 words at a time
+  mov ecx, 256
+  mov dx, 0x1F0
+  rep insw
+  pop ecx
+  loop .next_sector
+  ret
 
 ; fill until 510 bytes of sector with 0
 times 510-($ - $$) db 0
---
+++ src/kernel.asm
@@ -0,0 +1,27 @@
+; set 32 bit code 
+[BITS 32]
+global _start
+
+; set code and data offset addresses
+CODE_SEQ equ 0x08
+DATA_SEG equ 0x10
+
+_start:
+  mov ax, DATA_SEG
+  mov ds, ax
+  mov es, ax
+  mov fs, ax
+  mov gs, ax
+  mov ss, ax
+  mov ebp, 0x00200000
+  mov esp, ebp
+
+  ; Enable the A20 line
+  ; in, out - read and write to processer bus
+  in al, 0x92
+  or al, 2 
+  out 0x92, al 
+
+  jmp $
+
+
