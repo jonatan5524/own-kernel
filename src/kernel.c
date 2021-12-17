d0e18c4868a7e3bdd7433ec7c6cad4adf801855f
jonatan <jonatan5524@gmail.com>
2021-12-17 16:05:28 +0200

    changing current process

--- src/kernel.c
+++ src/kernel.c
@@ -179,12 +179,14 @@ void kernel_main()
   keyboard_init();
 
   struct process* process = 0;
-  int res = process_load("0:/blank.bin", &process);
+  int res = process_load_switch("0:/blank.bin", &process);
 
   if (res != ALL_OK)
   {
     panic("failed to load blank.bin");
   }
+
+  keyboard_push('A');
 
   task_run_first_ever_task();
 
--- src/task/process.h
+++ src/task/process.h
@@ -35,6 +35,8 @@ struct process
 };
 
 int process_load_for_slot(const char* filename, struct process** process, int process_slot);
+int process_load_switch(const char* filename, struct process** process);
+int process_switch(struct process* process);
 int process_load(const char* filename, struct process** process);
 struct process* process_current();
 struct process* process_get(int process_id);
