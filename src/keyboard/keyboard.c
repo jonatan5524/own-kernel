0b0666d0205b1a4d6baebc8ed744e448187064aa
jonatan <jonatan5524@gmail.com>
2021-12-25 20:15:45 +0200

    implementing capslock

--- src/keyboard/keyboard.c
+++ src/keyboard/keyboard.c
@@ -1,34 +1,26 @@
 #include "keyboard.h"
-#include "../task/task.h"
-#include "../status.h"
 #include "../kernel.h"
+#include "../status.h"
+#include "../task/task.h"
 #include "classic.h"
 
-static struct keyboard* keyboard_list_head = 0;
-static struct keyboard* keyboard_list_last = 0;
+static struct keyboard *keyboard_list_head = 0;
+static struct keyboard *keyboard_list_last = 0;
 
-void keyboard_init()
-{
-  keyboard_insert(classic_init());  
-}
+void keyboard_init() { keyboard_insert(classic_init()); }
 
-int keyboard_insert(struct keyboard* keyboard)
-{
+int keyboard_insert(struct keyboard *keyboard) {
   int res = 0;
 
-  if (keyboard->init == 0)
-  {
+  if (keyboard->init == 0) {
     res = -INVALID_ARGUMENT_ERROR;
     goto out;
   }
 
-  if (keyboard_list_last)
-  {
+  if (keyboard_list_last) {
     keyboard_list_last->next = keyboard;
     keyboard_list_last = keyboard;
-  }
-  else
-  {
+  } else {
     keyboard_list_head = keyboard;
     keyboard_list_last = keyboard;
   }
@@ -36,33 +28,37 @@ int keyboard_insert(struct keyboard* keyboard)
   res = keyboard->init();
 
 out:
-  return res; 
+  return res;
 }
 
-static int keyboard_get_tail_index(struct process* process)
-{
+void keyboard_set_capslock(struct keyboard *keyboard,
+                           KEYBOARD_CAPS_LOCK_STATE state) {
+  keyboard->capslock_state = state;
+}
+
+KEYBOARD_CAPS_LOCK_STATE keyboard_get_capslock(struct keyboard *keyboard) {
+  return keyboard->capslock_state;
+}
+
+static int keyboard_get_tail_index(struct process *process) {
   return process->keyboard.tail % sizeof(process->keyboard.buffer);
 }
 
-void keyboard_backspace(struct process* process)
-{ 
+void keyboard_backspace(struct process *process) {
   process->keyboard.tail -= 1;
 
   int real_index = keyboard_get_tail_index(process);
   process->keyboard.buffer[real_index] = 0x00;
 }
 
-void keyboard_push(char c)
-{
-  struct process* process = process_current();
+void keyboard_push(char c) {
+  struct process *process = process_current();
 
-  if (!process)
-  {
+  if (!process) {
     return;
-  } 
+  }
 
-  if (c == 0)
-  {
+  if (c == 0) {
     return;
   }
 
@@ -71,19 +67,16 @@ void keyboard_push(char c)
   process->keyboard.tail++;
 }
 
-char keyboard_pop()
-{
-  if (!task_current())
-  {
+char keyboard_pop() {
+  if (!task_current()) {
     return 0;
   }
 
-  struct process* process = task_current()->process;
+  struct process *process = task_current()->process;
   int real_index = process->keyboard.head % sizeof(process->keyboard.buffer);
   char c = process->keyboard.buffer[real_index];
 
-  if (c == 0x00)
-  {
+  if (c == 0x00) {
     return 0;
   }
 
--- src/keyboard/keyboard.h
+++ src/keyboard/keyboard.h
@@ -3,19 +3,26 @@
 
 #include "../task/process.h"
 
+#define KEYBOARD_CAPS_LOCK_ON 1
+#define KEYBOARD_CAPS_LOCK_OFF 0
+
+typedef int KEYBOARD_CAPS_LOCK_STATE;
 typedef int (*KEYBOARD_INIT_FUNCTION)();
 
-struct keyboard
-{
+struct keyboard {
   KEYBOARD_INIT_FUNCTION init;
   char name[20];
-  struct keyboard* next;
+  KEYBOARD_CAPS_LOCK_STATE capslock_state;
+  struct keyboard *next;
 };
 
 void keyboard_init();
-void keyboard_backspace(struct process* process);
+void keyboard_backspace(struct process *process);
 void keyboard_push(char c);
 char keyboard_pop();
-int keyboard_insert(struct keyboard* keyboard);
+int keyboard_insert(struct keyboard *keyboard);
+KEYBOARD_CAPS_LOCK_STATE keyboard_get_capslock(struct keyboard *keyboard);
+void keyboard_set_capslock(struct keyboard *keyboard,
+                           KEYBOARD_CAPS_LOCK_STATE state);
 
-#endif 
+#endif
